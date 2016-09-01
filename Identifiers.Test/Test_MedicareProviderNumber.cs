b0f05afacb3600eca42e6bc32366a56d0baf64c1
Angus Millar <angusmillar@iinet.net.au>
2016-09-01 17:11:28 +1000

    Added test cases and static IsValid method for MedicareProviderNumber

---
+++ Identifiers.Test/Test_MedicareProviderNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Identifiers;
+
+namespace Identifiers.Test
+{
+  [TestClass]
+  public class Test_MedicareProviderNumber
+  {
+    [TestMethod]
+    public void Test_ValidMedicareProviderNumber()
+    {
+      string MedicareProviderNumber = "2940675Y";      
+      Australian.MedicareProviderNumber oMedicareProviderNumber = new Australian.MedicareProviderNumber(MedicareProviderNumber);
+      Assert.IsTrue(oMedicareProviderNumber.IsValid());
+    }
+
+    [TestMethod]
+    public void Test_InValidMedicareProviderNumber()
+    {
+      string MedicareProviderNumber = "2940975Y";
+      Australian.MedicareProviderNumber oMedicareProviderNumber = new Australian.MedicareProviderNumber(MedicareProviderNumber);
+      Assert.IsFalse(oMedicareProviderNumber.IsValid());
+    }
+
+    [TestMethod]
+    public void Test_InValidMedicareProviderNumberStaticMethod()
+    {
+      string MedicareProviderNumber = "2940975Y";
+      Assert.IsFalse(Australian.MedicareProviderNumber.IsValid(MedicareProviderNumber));      
+    }
+
+    [TestMethod]
+    public void Test_ValidMedicareProviderNumberStaticMethod()
+    {
+      string MedicareProviderNumber = "2940675Y";
+      Assert.IsTrue(Australian.MedicareProviderNumber.IsValid(MedicareProviderNumber));
+    }
+
+
+  }
+}
--- Identifiers/Australian/MedicareProviderNumber.cs
+++ Identifiers/Australian/MedicareProviderNumber.cs
@@ -9,14 +9,14 @@ namespace Identifiers.Australian
   public class MedicareProviderNumber
   {
     //The Allowed Practice Location Values (PLV) and there positions
-    private char[] PracticeLocationValueChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
-                                             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 
-                                             'L', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'V', 'W', 
+    private char[] PracticeLocationValueChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                                             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
+                                             'L', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'V', 'W',
                                              'X', 'Y'};
-    
+
     //The Allowed Check Characters and there positions
     private char[] RemainderCheckCharacterChars = { 'Y', 'X', 'W', 'T', 'L', 'K', 'J', 'H', 'F', 'B', 'A' };
-    
+
     public string Value { get; set; }
     /// <summary>
     /// The six digit Stem portion on the Medicare Provider Number
@@ -25,7 +25,7 @@ namespace Identifiers.Australian
     {
       get
       {
-        if (this.IsValid)
+        if (this.IsValid())
           return this.Value.Substring(0, 6);
         else
           return string.Empty;
@@ -38,7 +38,7 @@ namespace Identifiers.Australian
     {
       get
       {
-        if (this.IsValid)
+        if (this.IsValid())
           return this.Value.Substring(6, 1);
         else
           return string.Empty;
@@ -51,7 +51,7 @@ namespace Identifiers.Australian
     {
       get
       {
-        if (this.IsValid)
+        if (this.IsValid())
           return this.Value.Substring(7, 1);
         else
           return string.Empty;
@@ -60,31 +60,54 @@ namespace Identifiers.Australian
     /// <summary>
     /// Validate the format and Check Character of the Medicare Provider Number
     /// </summary>
-    public bool IsValid
+    public bool IsValid()
     {
-      get
+      if (this.Value.Length == 8)
       {
-        if (this.Value.Length == 8)
+        if (Regex.IsMatch(this.Value.Substring(0, 6), @"^\d+$"))
         {
-          if (Regex.IsMatch(this.Value.Substring(0, 6), @"^\d+$"))
+          int PLVInteger = Array.IndexOf(PracticeLocationValueChars, this.Value.Substring(6, 1).ToCharArray()[0]);
+          if (PLVInteger > -1)
           {
-            int PLVInteger = Array.IndexOf(PracticeLocationValueChars, this.Value.Substring(6, 1).ToCharArray()[0]);
-            if (PLVInteger > -1)
-            {
-              int One = Convert.ToInt32(this.Value.Substring(0, 1));
-              int Two = Convert.ToInt32(this.Value.Substring(1, 1));
-              int Three = Convert.ToInt32(this.Value.Substring(2, 1));
-              int Four = Convert.ToInt32(this.Value.Substring(3, 1));
-              int Five = Convert.ToInt32(this.Value.Substring(4, 1));
-              int Six = Convert.ToInt32(this.Value.Substring(5, 1));
-              int CheckDigit = ((One * 3) + (Two * 5) + (Three * 8) + (Four * 4) + (Five * 2) + (Six) + (PLVInteger * 6)) % 11;
-              if (this.Value.Substring(7, 1) == RemainderCheckCharacterChars[CheckDigit].ToString())
-                return true;
-            }
+            int One = Convert.ToInt32(this.Value.Substring(0, 1));
+            int Two = Convert.ToInt32(this.Value.Substring(1, 1));
+            int Three = Convert.ToInt32(this.Value.Substring(2, 1));
+            int Four = Convert.ToInt32(this.Value.Substring(3, 1));
+            int Five = Convert.ToInt32(this.Value.Substring(4, 1));
+            int Six = Convert.ToInt32(this.Value.Substring(5, 1));
+            int CheckDigit = ((One * 3) + (Two * 5) + (Three * 8) + (Four * 4) + (Five * 2) + (Six) + (PLVInteger * 6)) % 11;
+            if (this.Value.Substring(7, 1) == RemainderCheckCharacterChars[CheckDigit].ToString())
+              return true;
           }
         }
-        return false;
       }
-    }                                           
+      return false;
+    }
+    /// <summary>
+    /// Validate the format and Check Character of the Medicare Provider Number
+    /// </summary>
+    /// <param name="MedicareProviderNumber"></param>
+    /// <returns></returns>
+    public static bool IsValid(string MedicareProviderNumber)
+    {
+      var ProviderNumber = new MedicareProviderNumber();
+      ProviderNumber.Value = MedicareProviderNumber;
+      return ProviderNumber.IsValid();
+    }
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="MedicareProviderNumber"></param>
+    public MedicareProviderNumber()
+    {      
+    }
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="MedicareProviderNumber"></param>
+    public MedicareProviderNumber(string MedicareProviderNumber)
+    {
+      this.Value = MedicareProviderNumber;
+    }
   }
 }
