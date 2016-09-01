b56d79103a088037b73e30ee2e3c37b6581f9e5f
Angus Millar <angusmillar@iinet.net.au>
2016-09-01 16:56:23 +1000

    Added static IsValid method for Medicare Numbers and new test cases for Medicare number. Also ad validation for the Irn number which was not happening before.

---
+++ Identifiers.Test/Test_MedicareNumber.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Identifiers;
+
+namespace Identifiers.Test
+{
+  [TestClass]
+  public class Test_MedicareNumber
+  {
+    [TestMethod]
+    public void Test_ValidMedicareNumberWithIrn()
+    {
+      string MedicareNumber = "4950481651";
+      string Irn = "1";
+      Australian.MedicareNumber oMedNumber = new Australian.MedicareNumber(MedicareNumber, Irn);
+      Assert.IsTrue(oMedNumber.IsValid());
+    }
+
+    [TestMethod]
+    public void Test_ValidMedicareNumberNoIrn()
+    {
+      string MedicareNumber = "4950481651";      
+      Australian.MedicareNumber oMedNumber = new Australian.MedicareNumber(MedicareNumber);
+      Assert.IsTrue(oMedNumber.IsValid());
+    }
+
+    [TestMethod]
+    public void Test_ValidMedicareNumberNoIrnStaticMethod()
+    {
+      string MedicareNumberNoIrn = "49504816511";      
+      Assert.IsTrue(Australian.MedicareNumber.IsValid(MedicareNumberNoIrn));
+    }
+
+    [TestMethod]
+    public void Test_ValidMedicareNumberWithIrnStaticMethod()
+    {
+      string MedicareNumberWithIrn = "49504816511";      
+      Assert.IsTrue(Australian.MedicareNumber.IsValid(MedicareNumberWithIrn));
+    }
+
+    [TestMethod]
+    public void Test_InValidMedicareNumberWithIrnStaticMethod()
+    {
+      string MedicareNumberWithIrn = "495048135195";
+      Assert.IsFalse(Australian.MedicareNumber.IsValid(MedicareNumberWithIrn));
+    }
+
+    [TestMethod]
+    public void Test_InValidMedicareNumberNoIrnStaticMethod()
+    {
+      string MedicareNumberNoIrn = "2950311931";
+      Assert.IsFalse(Australian.MedicareNumber.IsValid(MedicareNumberNoIrn));
+    }
+
+  }
+}
--- Identifiers/Australian/MedicareNumber.cs
+++ Identifiers/Australian/MedicareNumber.cs
@@ -24,38 +24,52 @@ namespace Identifiers.Australian
     /// Validates the Medicare Number by calculating it's check digit
     /// returns true if check digit is correct  
     /// </summary>
-    public bool IsValid
+    public bool IsValid()
     {
-      get
+      if (!string.IsNullOrEmpty(this.Value))
       {
-        if (!string.IsNullOrEmpty(this.Value))
+        if (this.Value.Length == 10)
         {
-          if (this.Value.Length == 10)
+          if (Regex.IsMatch(this.Value, @"^\d+$"))
           {
-            if (Regex.IsMatch(this.Value, @"^\d+$"))
+            int One = Convert.ToInt32(this.Value.Substring(0, 1));
+            //The first Char must be in the range 2-6
+            if (One > 1 && One < 7)
             {
-              int One = Convert.ToInt32(this.Value.Substring(0, 1));
-              //The first Char must be in the range 2-6
-              if (One > 1 && One < 7)
+              int Two = Convert.ToInt32(this.Value.Substring(1, 1));
+              int Three = Convert.ToInt32(this.Value.Substring(2, 1));
+              int Four = Convert.ToInt32(this.Value.Substring(3, 1));
+              int Five = Convert.ToInt32(this.Value.Substring(4, 1));
+              int Six = Convert.ToInt32(this.Value.Substring(5, 1));
+              int Seven = Convert.ToInt32(this.Value.Substring(6, 1));
+              int Eight = Convert.ToInt32(this.Value.Substring(7, 1));
+              int Nine = Convert.ToInt32(this.Value.Substring(8, 1));
+              int CheckDigit = ((One) + (Two * 3) + (Three * 7) + (Four * 9) + (Five) + (Six * 3) + (Seven * 7) + (Eight * 9));
+              CheckDigit = CheckDigit % 10;
+              if (Nine == CheckDigit)
               {
-                int Two = Convert.ToInt32(this.Value.Substring(1, 1));
-                int Three = Convert.ToInt32(this.Value.Substring(2, 1));
-                int Four = Convert.ToInt32(this.Value.Substring(3, 1));
-                int Five = Convert.ToInt32(this.Value.Substring(4, 1));
-                int Six = Convert.ToInt32(this.Value.Substring(5, 1));
-                int Seven = Convert.ToInt32(this.Value.Substring(6, 1));
-                int Eight = Convert.ToInt32(this.Value.Substring(7, 1));
-                int Nine = Convert.ToInt32(this.Value.Substring(8, 1));
-                int CheckDigit = ((One) + (Two * 3) + (Three * 7) + (Four * 9) + (Five) + (Six * 3) + (Seven * 7) + (Eight * 9));
-                CheckDigit = CheckDigit % 10;
-                if (Nine == CheckDigit)
+                int TempInt;
+                if (!string.IsNullOrWhiteSpace(this.IRN))
+                {
+                  if (this.IRN.Length == 1 && int.TryParse(this.IRN, out TempInt))
+                  {
+                    return true;
+                  }
+                  else
+                  {
+                    return false;
+                  }
+                }
+                else
+                {
                   return true;
-              }
+                }               
+              }                
             }
           }
         }
-        return false;
       }
+      return false;
     }
     /// <summary>
     /// MedicareNumber Constructor
@@ -64,6 +78,19 @@ namespace Identifiers.Australian
     {
       this.Expiry = DateTime.MinValue;
     }
+
+    /// <summary>
+    /// MedicareNumber Constructor
+    /// </summary>
+    /// <param name="Value"></param>
+    /// <param name="Irn"></param>
+    public MedicareNumber(string Value)
+    {
+      this.Value = Value;
+      this.IRN = string.Empty;
+      this.Expiry = DateTime.MinValue;
+    }
+    
     /// <summary>
     /// MedicareNumber Constructor
     /// </summary>
@@ -89,5 +116,48 @@ namespace Identifiers.Australian
       this.IRN = Irn;
       this.Expiry = Expiry;
     }
+
+    #region Public Static Properties
+
+    public static bool IsValid(string MedicareNumber)
+    {
+      MedicareNumber Id = null;
+      string Number = string.Empty;
+      string Irn = string.Empty;
+      if (MedicareNumber.Length == 11)
+      {
+        Number = MedicareNumber.Substring(0, 10);
+        Irn = MedicareNumber.Substring(10, 1);
+        Id = new MedicareNumber(Number, Irn);
+        return Id.IsValid();
+      }
+      else if (MedicareNumber.Length == 10)
+      {
+        Id = new MedicareNumber(MedicareNumber);
+        return Id.IsValid();
+      }
+      else
+      {
+        return false;
+      }      
+    }
+
+
+    /// <summary>
+    /// Static properties for HL7 V2 Messages
+    /// </summary>
+    public static class Hl7V2
+    {
+      /// <summary>
+      ///e.g: PID-3.4 (Ordering Provider)
+      /// </summary>
+      public static string AssigningAuthority { get { return "AUSHIC"; } }
+      /// <summary>
+      ///e.g: PID-3.5 (Ordering Provider)
+      /// </summary>
+      public static string IdentifierTypeCode { get { return "MC"; } }
+    }
+
+    #endregion
   }
 }
